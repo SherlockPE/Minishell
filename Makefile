fbf00e45b976bb74e55d402fb0edf881e47d3d01
albartol <albartol@student.42madrid.com>
2024-03-05 20:19:16 +0100

    Added headders

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: albartol <albartol@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/03/05 18:53:11 by albartol          #+#    #+#              #
-#    Updated: 2024/03/05 19:08:26 by albartol         ###   ########.fr        #
+#    Updated: 2024/03/05 19:35:19 by albartol         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,16 +22,16 @@ NAME=minishell
 
 LIBFT=lib/libft/libft.a
 
-all: $(LIBFT) $(NAME)
+all: $(NAME)
 
-$(LIBFT):
-	$(MAKE) -C lib/libft
-
-$(NAME): $(OBJ)
-	$(CC) $(CFLAGS) $^ -Llib/libft -lft -o $@
+$(NAME): include/minishell.h $(OBJ) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJ) -Llib/libft -lft -o $@
 
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFT):
+	$(MAKE) -C lib/libft
 
 clean:
 	$(MAKE) clean -C lib/libft
--- include/minishell.h
+++ include/minishell.h
@@ -6,26 +6,53 @@
 /*   By: albartol <albartol@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 18:58:51 by albartol          #+#    #+#             */
-/*   Updated: 2024/03/05 19:05:21 by albartol         ###   ########.fr       */
+/*   Updated: 2024/03/05 20:10:00 by albartol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-// for printf
+// for printf, readline, perror
 # include <stdio.h>
-// for malloc, free & exit
+// for strerror
+# include <string.h>
+// for malloc, free, exit, getenv
 # include <stdlib.h>
-// for write
+// for write, access, close, read, getcwd, chdir,
+// unlink, execve, dup, dup2, pipe, isatty, ttyname, ttyslot
 # include <unistd.h>
-// for open, close & read
+// stat, lstat, fstat
+# include <sys/stat.h>
+// for read, wait3, wait4
+#include <sys/types.h>
+// for read
+#include <sys/uio.h>
+// for open
 # include <fcntl.h>
-// for signal, sigaction & kill
+// for signal, sigaction, kill
 # include <signal.h>
-// for fork & kill
+// for fork, kill
 # include <sys/types.h>
-// for waitpid, wait, waitpid, wait3 & wait4
+// for waitpid, wait, waitpid, wait3, wait4
 # include <sys/wait.h>
+// for wait3, wait4
+#include <sys/time.h>
+// for wait3, wait4
+#include <sys/resource.h>
+// for readline, rl_on_new_line, rl_redisplay, add_history
+# include <readline/readline.h> /* rl_clear_history, rl_redisplay ? */
+// for readline, rl_on_new_line, rl_redisplay, add_history
+# include <readline/history.h> /* rl_clear_history, rl_redisplay ? */
+// for opendir, readdir, closedir
+# include <dirent.h>
+// for ioctl
+# include <sys/ioctl.h>
+// for tcsetattr, tcgetattr
+# include <termios.h>
+// for tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+# include <curses.h>
+// for tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+# include <term.h>
 
 #endif
