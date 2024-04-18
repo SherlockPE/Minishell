43afe12ab6fdccd1588b58290cc05607132f27d6
albartol <albartol@student.42madrid.com>
2024-04-18 14:27:17 +0200

    Minor change

--- src/executor/ft_exec_one.c
+++ src/executor/ft_exec_one.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exec_one.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: albartol <albartol@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 14:03:22 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/04/17 20:35:45 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/04/18 14:26:31 by albartol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ static int	output_fd(t_pipe *com, t_shell *data)
 	int		fd;
 	int		i;
 
+	fd = 0;
 	if (com->output.file_name)
 	{
 		if (com->output.type == TRUNC)
 			fd = open(com->output.file_name, O_WRONLY | O_TRUNC);
 		else if (com->output.type == APPEND)
 			fd = open(com->output.file_name, O_WRONLY | O_APPEND);
+		if (fd == 0)
+			return (EXIT_SUCCESS);
 		if (fd == -1)
 			return (ft_exit_funct("open", EXIT_FAILURE));
 		i = dup2(fd, STDOUT_FILENO);
@@ -51,28 +54,41 @@ static int	change_fd(t_pipe *com, t_shell *data)
 	return (output_fd(com, data));
 }
 
-static int	save_fd(int *fd)
+static int	save_fd(int *fd, t_pipe *com)
 {
-	fd[0] = dup(STDIN_FILENO);
-	if (fd[0] == -1)
-		return (ft_exit_funct("dup: old_stdin", EXIT_FAILURE));
-	fd[1] = dup(STDOUT_FILENO);
-	if (fd[1] == -1)
+	if (com->input.file_name)
 	{
-		close(fd[0]);
-		return (ft_exit_funct("dup: old_stdout", EXIT_FAILURE));
+		fd[0] = dup(STDIN_FILENO);
+		if (fd[0] == -1)
+			return (ft_exit_funct("dup: old_stdin", EXIT_FAILURE));
+	}
+	if (com->output.file_name)
+	{
+		fd[1] = dup(STDOUT_FILENO);
+		if (fd[1] == -1)
+		{
+			if (com->input.file_name)
+				close(fd[0]);
+			return (ft_exit_funct("dup: old_stdout", EXIT_FAILURE));
+		}
 	}
 	return (EXIT_SUCCESS);
 }
 
-static void	restore_fd(int *fd)
+static void	restore_fd(int *fd, t_pipe *com)
 {
-	if (dup2(fd[1], STDOUT_FILENO) == -1)
-		perror("dup2: old_stdout");
-	if (dup2(fd[0], STDIN_FILENO) == -1)
-		perror("dup2: old_stdin");
-	close(fd[1]);
-	close(fd[0]);
+	if (com->input.file_name)
+	{
+		if (dup2(fd[0], STDIN_FILENO) == -1)
+			perror("dup2: old_stdin");
+		close(fd[0]);
+	}
+	if (com->output.file_name)
+	{
+		if (dup2(fd[1], STDOUT_FILENO) == -1)
+			perror("dup2: old_stdout");
+		close(fd[1]);
+	}
 }
 
 void	ft_exec_one(t_shell *data)
@@ -86,15 +102,15 @@ void	ft_exec_one(t_shell *data)
 		perror("malloc");
 		return ;
 	}
-	if (save_fd(&fd[0]))
+	if (save_fd(&fd[0], data->com))
 		return ;
 	if (change_fd(data->com, data))
 	{
-		restore_fd(&fd[0]);
+		restore_fd(&fd[0], data->com);
 		return ;
 	}
 	ft_exec_command(argv, data);
-	restore_fd(&fd[0]);
+	restore_fd(&fd[0], data->com);
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &data->conf) == -1)
 		perror("tcsetattr");
 }
