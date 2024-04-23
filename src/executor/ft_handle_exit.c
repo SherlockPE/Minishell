707a5f7f469b670f51659b90ce1ed808641e5110
Fabricio <flopez-r@student.42madrid.com>
2024-04-23 15:10:35 +0200

    Added some signal messages and exit code to minishell

--- src/executor/ft_handle_exit.c
+++ src/executor/ft_handle_exit.c
@@ -6,20 +6,31 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:48:24 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/04/22 14:03:07 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/04/23 15:06:49 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+static void	print_sig_message(char *message, int exit_code)
+{
+	ft_putstr_fd(message, STDERR);
+	ft_putnbr_fd(exit_code, STDERR);
+	ft_putchar_fd('\n', STDERR);
+}
+
 static void	print_signal_name(int exit_code)
 {
 	if (exit_code == SIGSEGV)
-		ft_putstr_fd("Segmentation fault :", STDERR);
+		print_sig_message("Segmentation fault :", exit_code);
 	else if (exit_code == SIGTERM)
-		ft_putstr_fd("Terminated :", STDERR);
-	ft_putnbr_fd(exit_code, STDERR);
-	ft_putchar_fd('\n', STDERR);
+		print_sig_message("Terminated :", exit_code);
+	else if (exit_code == SIGQUIT)
+		print_sig_message("Quit :", exit_code);
+	else if (exit_code == SIGKILL)
+		print_sig_message("Killed :", exit_code);
+	else if (exit_code == SIGSTOP)
+		print_sig_message("Stoped :", exit_code);
 }
 
 void	ft_handle_exit(t_shell *data, int wstatus)
